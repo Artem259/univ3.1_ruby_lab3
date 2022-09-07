a0fc3f83132fc6ecdb8953e2c296706fd8c72645
Artem Poliakov <artem4250@gmail.com>
2022-09-07 23:27:20 +0300

    task3.rb

---
+++ task3.rb
@@ -0,0 +1,29 @@
+def fact(n)
+  n > 1 ? n*fact(n-1) : 1
+end
+
+print 'Enter x n values (space-separated):'
+str = gets
+exit if str.nil? || str.empty?
+str.chomp!
+x, n = str.split(' ')
+x, n = x.to_f, n.to_i
+print "\nInput:\n x = #{x}\n n = #{n}\n\n"
+
+current = 1.0; result = 1.0
+8.times do
+  current = current / 3
+  result += current
+end
+
+print "\n  ~~ 2 ~~\n   ", result, "\n"
+
+#------------------------------------------------------------#
+
+current = 1.0; result = 1.0
+(1..n).each do|i|
+  current = current * x / i
+  result += current
+end
+
+print "\n  ~~ 3 ~~\n   ", result, "\n"
